a85ae48befe20b9c0c557d1538900eca5c34d1f9
mangoleafdisease <sambadu2022@gmail.com>
2023-05-30 09:44:19 +0800

    first commit

--- api/main.py
+++ api/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("../models/7")
+MODEL = tf.keras.models.load_model("../models/9/1")
 
 CLASS_NAMES = ["Anthracnose", "Bacterial Canker","Gail Midge", "Healthy", "Powdery Mildew", "Sooty Mould" ]
 RECOMMENDATIONS = None
